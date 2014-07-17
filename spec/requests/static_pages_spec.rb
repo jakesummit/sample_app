c5bae51d0e235f54fa63ac5d175dc5b99c65c490
Jake Kelly <jake.kelly10@gmail.com>
2014-07-17 17:40:47 +0100

    Make a basic User model w/ secure passwords

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,33 +6,71 @@ describe "Static pages" do
 
 	subject { page }
 
+	shared_examples_for "all static pages" do
+		it { should have_selector('h1', text: heading) }
+	end
+
 	describe "Home page" do
 		before { visit root_path }
 
+		let(:heading) { 'Sample App' }
+
+		it { should have_selector('h1', text: heading) }
 		it { should have_content('Sample App') }
 		it { should have_title("#{base_title}") }
 		it { should_not have_title('| Home') }
+
+		it_should_behave_like "all static pages"
 	end
 
 	describe "Help page" do
 		before { visit help_path }
 
+		let(:heading) { 'Help' }
+
+		it { should have_selector('h1', text: heading) }
 		it { should have_content('Help') }
 		it { should have_title("#{base_title} | Help") }
+		it_should_behave_like "all static pages"
 	end
 
 	describe "About page" do
 		before { visit about_path }
 
+		let(:heading) { 'About Us' }
+
+		it { should have_selector('h1', text: heading) }
 		it { should have_content('About') }
 		it { should have_title("#{base_title} | About Us") }
+		it_should_behave_like "all static pages"
 	end
 
 	describe "Contact page" do
 	    before { visit contact_path }
 
+	    let(:heading) { 'Contact' }
+
+		it { should have_selector('h1', text: heading) }
 	    it { should have_content('Contact') }
+	    it { should have_selector('h1', text: 'Contact') }
 	    it { should have_title("#{base_title} | Contact") }
+	    it_should_behave_like "all static pages"
+	end
+
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		expect(page).to have_title("#{base_title} | About Us")
+		click_link "Help"
+		expect(page).to have_title("#{base_title} | Help")
+		click_link "Contact"
+		expect(page).to have_title("#{base_title} | Contact")
+		click_link "Home"
+		expect(page).to have_title("#{base_title}")
+		click_link "Sign up now!"
+		expect(page).to have_title("#{base_title} | Sign up")
+		click_link "sample app"
+		expect(page).to have_title("#{base_title}")
 	end
 
 end
